77c58b483e27ae6edf9198b889afae04da023a99
Alek097 <petrovskii097@gmail.com>
2016-08-26 20:50:33 +0300

    Initial commit: added unity container

---
+++ Solo_grupp/API/Infrastructure/UnityHttpControllerActivator.cs
@@ -0,0 +1,33 @@
+﻿namespace API.Infrastructure
+{
+    #region Using
+    using System;
+    using System.Diagnostics;
+    using System.Net.Http;
+    using System.Web.Http.Controllers;
+    using System.Web.Http.Dispatcher;
+    using Microsoft.Practices.Unity;
+
+    #endregion
+    public sealed class UnityHttpControllerActivator : IHttpControllerActivator
+    {
+        private readonly IUnityContainer _container;
+
+        public UnityHttpControllerActivator(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public IHttpController Create(HttpRequestMessage request, HttpControllerDescriptor controllerDescriptor, Type controllerType)
+        {
+            IHttpController result = null;
+
+            if (controllerType != null)
+            {
+                result = _container.Resolve(controllerType) as IHttpController;
+            }
+
+            return result;
+        }
+    }
+}
