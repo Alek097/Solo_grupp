faeedd0fb97ba01d9861fc93b2264c75a44cc084
Alek097 <petrovskii097@gmail.com>
2016-08-29 00:42:08 +0300

    Initial commit: added logging project

---
+++ Solo_grupp/API.Logging/ILogger.cs
@@ -0,0 +1,22 @@
+﻿namespace API.Logging
+{
+
+	#region Using
+	using System;
+	#endregion
+	public interface ILogger
+	{
+		void WriteInformation(string message);
+		void WriteInformation(Exception ex, string message);
+		void WriteWarning(string message);
+		void WriteWarning(Exception ex, string message);
+		void WriteError(string message);
+		void WriteError(Exception ex, string message);
+		void WriteFatal(string message);
+		void WriteFatal(Exception ex, string message);
+		void WriteVerbose(string message);
+		void WriteVerbose(Exception ex, string message);
+		void WriteDebug(string message);
+		void WriteDebug(Exception ex, string message);
+	}
+}
