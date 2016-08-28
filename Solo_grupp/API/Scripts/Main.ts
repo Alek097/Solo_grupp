02983800b24543e1d89aad7692e6262305856a50
Alek097 <petrovskii097@gmail.com>
2016-08-28 23:17:48 +0300

    fix

--- Solo_grupp/API/Scripts/Main.ts
+++ Solo_grupp/API/Scripts/Main.ts
@@ -1,6 +1,18 @@
 ﻿import './Pages/PagesModule.ts'
+import './Common/CommonModule.ts'
 
 angular.module('main',
     [
+        'common',
         'pages'
     ]);
+    //.config(($routeProvider: ng.route.IRouteProvider) =>
+    //{
+    //    $routeProvider.when('registration/',
+    //        <ng.route.IRoute>
+    //        {
+    //            templateUrl: './Pages/Registration/RegistrationView.html',
+    //            controller: 'RegistrationController',
+    //            controllerAs: 'registr'
+    //        });
+    //});
