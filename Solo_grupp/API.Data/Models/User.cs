b343feaa863099b0ae56c07f5457d6193040ff84
Alek097 <petrovskii097@gmail.com>
2016-08-29 15:17:39 +0300

    Initial commit: added constructors

--- Solo_grupp/API.Data/Models/User.cs
+++ Solo_grupp/API.Data/Models/User.cs
@@ -1,9 +1,10 @@
 ﻿namespace API.Data.Models
 {
 	#region Using
-	using System;
 	using System.Collections.Generic;
 	using Microsoft.AspNet.Identity.EntityFramework;
+	using API.Models;
+	using System.Text;
 	#endregion
 	public class User : IdentityUser
 	{
@@ -14,5 +15,34 @@
 		public string Adress { get; set; }
 		public virtual ICollection<Resolution> Permission { get; set; }
 		public virtual ICollection<News> News { get; set; }
+
+		public User()
+		{
+
+		}
+		public User(RegistartionModel model)
+		{
+			this.firstUpper(model.FirstName);
+			this.firstUpper(model.LastName);
+			this.firstUpper(model.Patronymic);
+
+			this.FirstName = model.FirstName;
+			this.LastName = model.LastName;
+			this.Patronymic = model.Patronymic;
+			this.FullName = string.Format("{0} {1} {2}",
+				model.LastName,
+				model.FirstName,
+				model.Patronymic);
+			this.Adress = model.Adress;
+			this.Email = model.Email;
+			this.PhoneNumber = model.PhoneNumber;
+		}
+
+		private void firstUpper(string value)
+		{
+			StringBuilder builder = new StringBuilder(value);
+			builder[0] = char.ToUpper(builder[0]);
+			value = builder.ToString();
+		}
 	}
 }
