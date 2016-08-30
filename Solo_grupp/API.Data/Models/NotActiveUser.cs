5c1ea5a22aa9ccbccf140b4e27fad0c0c7a3aa63
Alek097 <petrovskii097@gmail.com>
2016-08-30 12:31:52 +0300

    Initial commit: added NotActiveUser and Salt

---
+++ Solo_grupp/API.Data/Models/NotActiveUser.cs
@@ -0,0 +1,46 @@
+﻿namespace API.Data.Models
+{
+	#region Using
+	using API.Models;
+	using System;
+	using System.Text;
+	#endregion
+	public class NotActiveUser
+	{
+		public Guid Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Patronymic { get; set; }
+		public string Password { get; set; }
+		public string Adress { get; set; }
+		public string Email { get; set; }
+		public string PhoneNumber { get; set; }
+
+		public NotActiveUser()
+		{
+
+		}
+		public NotActiveUser(RegistrationModel model)
+		{
+			this.Id = Guid.NewGuid();
+			this.firstUpper(model.FirstName);
+			this.firstUpper(model.LastName);
+			this.firstUpper(model.Patronymic);
+
+			this.FirstName = model.FirstName;
+			this.LastName = model.LastName;
+			this.Patronymic = model.Patronymic;
+			this.Adress = model.Adress;
+			this.Email = model.Email;
+			this.PhoneNumber = model.PhoneNumber;
+			this.Password = model.Password;
+		}
+		private void firstUpper(string value)
+		{
+			StringBuilder builder = new StringBuilder(value);
+			builder[0] = char.ToUpper(builder[0]);
+			value = builder.ToString();
+		}
+	}
+}
+
---
+++ Solo_grupp/API.Data/Models/Salt.cs
@@ -0,0 +1,20 @@
+﻿namespace API.Data.Models
+{
+	#region Using
+	using System;
+	#endregion
+	public class Salt
+	{
+		public Guid Id { get; set; }
+		public string Value { get; set; }
+
+		public Salt()
+		{
+			this.Id = Guid.NewGuid();
+			this.Value = string.Format("{0}-{1}-{2}",
+				Guid.NewGuid(),
+				Guid.NewGuid(),
+				Guid.NewGuid());
+		}
+	}
+}
