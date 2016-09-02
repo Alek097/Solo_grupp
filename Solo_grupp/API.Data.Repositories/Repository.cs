f249013e130bef44f2f9a044bcbe7629df21010a
Alek097 <petrovskii097@gmail.com>
2016-09-02 17:43:20 +0300

    fix

--- Solo_grupp/API.Data.Repositories/Repository.cs
+++ Solo_grupp/API.Data.Repositories/Repository.cs
@@ -5,7 +5,7 @@
 		protected const string DNS = "";
 		public string MovedError(int httpCode, string message)
 		{
-			return string.Format("{0}/#/error?httpCode={1}&message={2}", DNS, httpCode, message);
+			return string.Format("{0}/#/Error/{1}/{2}", DNS, httpCode, message);
 		}
 	}
 }
