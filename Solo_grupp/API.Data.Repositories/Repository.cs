2338578d035b68f23a18b85d6beea48f5ee9884d
Alek097 <petrovskii097@gmail.com>
2016-09-03 23:25:27 +0300

    Initial commit: added MovedMessage method

--- Solo_grupp/API.Data.Repositories/Repository.cs
+++ Solo_grupp/API.Data.Repositories/Repository.cs
@@ -7,5 +7,9 @@
 		{
 			return string.Format("{0}/#/Error/{1}/{2}", DNS, httpCode, message);
 		}
+		public string MovedMessage(string message)
+		{
+			return string.Format("{0}/#/Message/{2}", DNS, message);
+		}
 	}
 }
