75a4c5ce76f619a2e642f280323109d86ea03918
Alek097 <petrovskii097@gmail.com>
2016-09-05 21:53:12 +0300

    Initial commit: added UserInformation

---
+++ Solo_grupp/API.Models/UserInformation.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Models
+{
+	public class UserInformation
+	{
+		public string Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Patronymic { get; set; }
+		public string FullName { get; set; }
+		public string Adress { get; set; }
+		public string Email { get; set; }
+	}
+}
