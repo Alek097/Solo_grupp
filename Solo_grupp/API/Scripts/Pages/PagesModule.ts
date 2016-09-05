d6510d6ef72b1e34b3647851dbff6204c234e333
Alek097 <petrovskii097@gmail.com>
2016-09-05 20:41:59 +0300

    Initial commit: added SignInModule.ts and register

--- Solo_grupp/API/Scripts/Pages/PagesModule.ts
+++ Solo_grupp/API/Scripts/Pages/PagesModule.ts
@@ -1,10 +1,12 @@
 ﻿import './Error/ErrorModule.ts'
 import './SignUp/SignUpModule.ts'
 import './Message/MessageModule.ts'
+import './SignIn/SignInModule.ts'
 
 angular.module('pages',
     [
         'error',
         'signUp',
-        'message'
+        'message',
+        'signIn'
     ]);
