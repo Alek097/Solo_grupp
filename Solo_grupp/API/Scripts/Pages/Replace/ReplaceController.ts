aeae4b0e97a2d979e3db44e793a6bd1af9473e01
Alek097 <petrovskii097@gmail.com>
2016-09-08 22:34:15 +0300

    Initial commit: added module template

---
+++ Solo_grupp/API/Scripts/Pages/Replace/ReplaceController.ts
@@ -0,0 +1,39 @@
+﻿import {ReplaceService} from './ReplaceService.ts'
+import {AuthorizeService} from '../../Common/Authorize/AuthorizeService.ts'
+import {User} from '../../Common/Models/User.ts'
+
+export class ReplaceController {
+
+    public error: string;
+
+    public static $inject: string[] =
+    [
+        'replaceService',
+        'authorizeService',
+        '$routeParams'
+    ];
+
+    constructor(
+        private service: ReplaceService,
+        authorizeService: AuthorizeService,
+        params: ng.route.IRouteParamsService
+    ) {
+        authorizeService.Authentification()
+            .success((data: User) => {
+                if (data == undefined) {
+                    return;
+                }
+                else {
+                    window.location.href = '/#/Home';
+                }
+            })
+            .error(() => {
+                window.location.href = '/#/Home';
+            });
+
+        this.error = params['message'];
+
+        if (this.error == undefined)
+            this.error = '';
+    }
+}
---
+++ Solo_grupp/API/Scripts/Pages/Replace/ReplaceService.ts
@@ -0,0 +1,16 @@
+﻿export class ReplaceService {
+    public static $inject: string[] =
+    [
+        '$http'
+    ];
+
+    constructor(
+        private http: ng.IHttpService
+    ) {
+
+    }
+
+    public Replace(email: string): ng.IHttpPromise<any> {
+        return this.http.post('/api/user/replace', email);
+    }
+}
