a0362e6f08de148163dc2093d916222d4958e0fc
Alek097 <petrovskii097@gmail.com>
2016-09-08 23:47:42 +0300

    Initial commit: added emailValidation and submit method

--- Solo_grupp/API/Scripts/Pages/Replace/ReplaceController.ts
+++ Solo_grupp/API/Scripts/Pages/Replace/ReplaceController.ts
@@ -1,10 +1,15 @@
 ﻿import {ReplaceService} from './ReplaceService.ts'
 import {AuthorizeService} from '../../Common/Authorize/AuthorizeService.ts'
 import {User} from '../../Common/Models/User.ts'
+import {MoveTo} from '../../Common/Models/MoveTo.ts'
+import {RepositoryResult} from '../../Common/Models/RepositoryResult.ts'
 
 export class ReplaceController {
 
     public error: string;
+
+    private email: string;
+    private elem: JQuery;
 
     public static $inject: string[] =
     [
@@ -35,5 +40,63 @@ export class ReplaceController {
 
         if (this.error == undefined)
             this.error = '';
+    }
+
+    public emailValidate(): boolean {
+        let email: string = angular.element('#email').val();
+
+        if (email === undefined)
+            return;
+
+        this.elem = angular.element('#error-email');
+
+        if (email == undefined) {
+            this.writeError('Введите Email');
+            return false;
+        }
+
+        this.email = email;
+
+        if (email.length === 0) {
+            this.writeError('Введите Email');
+            return false;
+        }
+        else {
+            this.clearError();
+        }
+
+        let Regex: RegExp = new RegExp('^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}$');
+
+        if (Regex.test(email)) {
+            this.clearError();
+            return true;
+        }
+        else {
+            this.writeError('Введите Email в правильном формате');
+            return false;
+        }
+    }
+
+    public submit(): void {
+        let valid: boolean = this.emailValidate();
+
+        if (valid) {
+            this.service.Replace(this.email)
+                .success((data) => {
+                    if (data.Responce.IsMoving) {
+                        window.location.href = data.Responce.Location;
+                    }
+                    else {
+                        //NextSlide
+                    }
+                });
+        }
+    }
+
+    private writeError(text: string): void {
+        this.elem.text(text + '.');
+    }
+    private clearError(): void {
+        this.elem.text('');
     }
 }
