fce083061913f77cfe7d6804cdfdee831f6b451d
Alek097 <petrovskii097@gmail.com>
2016-09-10 16:13:23 +0300

    Initial commit: added second form and methods for validate him

--- Solo_grupp/API/Scripts/Pages/Replace/ReplaceController.ts
+++ Solo_grupp/API/Scripts/Pages/Replace/ReplaceController.ts
@@ -3,13 +3,16 @@ import {AuthorizeService} from '../../Common/Authorize/AuthorizeService.ts'
 import {User} from '../../Common/Models/User.ts'
 import {MoveTo} from '../../Common/Models/MoveTo.ts'
 import {RepositoryResult} from '../../Common/Models/RepositoryResult.ts'
+import {Replace} from '../../Common/Models/Replace.ts'
 
 export class ReplaceController {
 
     public error: string;
+    public isFirst: boolean = true;
 
     private email: string;
     private elem: JQuery;
+    private model: Replace;
 
     public static $inject: string[] =
     [
@@ -77,6 +80,78 @@ export class ReplaceController {
         }
     }
 
+    public passwordValidate(): boolean {
+        let password: string = angular.element('#password').val();
+
+        this.model.Password = password;
+
+        this.elem = angular.element('#error-password');
+
+        if (password == undefined || password.length == 0) {
+            this.writeError('Введите пароль')
+            return false;
+        }
+        else {
+            this.clearError();
+        }
+
+        if (password.length < 5) {
+            this.writeError('Пароль меньше 5 символов');
+            return false;
+        }
+        else {
+            this.clearError();
+            return true;
+        }
+    }
+
+    public repeatedPasswordValidate(): boolean {
+        let repeatedPassword: string = angular.element('#repeatedPassword').val();;
+
+        this.model.RepeatedPassword = repeatedPassword;
+
+        this.elem = angular.element('#error-repeatedPassword');
+
+        if (repeatedPassword == undefined) {
+            this.writeError('Повторите пароль');
+        }
+
+        if (repeatedPassword !== this.model.Password) {
+            this.writeError('Пароли не совпадают');
+            return false;
+        }
+        else {
+            this.clearError();
+            return true;
+        }
+    }
+
+    public replaceCodeValidate(): boolean {
+        let replaceCode: string = angular.element('#replaceCode').val();
+
+        this.model.ReplaceCode = replaceCode;
+
+        this.elem = angular.element('#error-replaceCode');
+
+        if (replaceCode == undefined || replaceCode.length == 0) {
+            this.writeError('Повторите пароль');
+            return false;
+        }
+        else {
+            this.clearError();
+        }
+
+        let regex: RegExp = new RegExp('[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}');
+
+        if (regex.test(replaceCode)) {
+            this.clearError();
+        }
+        else {
+            this.writeError('Неправильный код');
+            return false;
+        }
+    }
+
     public submit(): void {
         let valid: boolean = this.emailValidate();
 
@@ -87,9 +162,19 @@ export class ReplaceController {
                         window.location.href = data.Responce.Location;
                     }
                     else {
-                        //NextSlide
+                        this.isFirst = false;
                     }
                 });
+        }
+    }
+
+    public submitPass(): void {
+        let valid: boolean = this.replaceCodeValidate();
+        valid = valid && this.passwordValidate();
+        valid = valid && this.repeatedPasswordValidate();
+
+        if (valid) {
+
         }
     }
 
