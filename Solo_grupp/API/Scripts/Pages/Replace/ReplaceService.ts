627459fbbcae99cfdc123505e4e89cbf6c31ee23
Alek097 <petrovskii097@gmail.com>
2016-09-10 18:05:24 +0300

    Initial commit: added ReplacePassword method

--- Solo_grupp/API/Scripts/Pages/Replace/ReplaceService.ts
+++ Solo_grupp/API/Scripts/Pages/Replace/ReplaceService.ts
@@ -1,5 +1,6 @@
 ﻿import {MoveTo} from '../../Common/Models/MoveTo.ts'
 import {RepositoryResult} from '../../Common/Models/RepositoryResult.ts'
+import {Replace} from '../../Common/Models/Replace.ts'
 
 export class ReplaceService {
     public static $inject: string[] =
@@ -15,5 +16,8 @@ export class ReplaceService {
 
     public Replace(email: string): ng.IHttpPromise<RepositoryResult<MoveTo>> {
         return this.http.post('/api/user/replace?email=' + email, null);
+    }
+    public ReplacePassword(model: Replace): ng.IHttpPromise<RepositoryResult<MoveTo>> {
+        return this.http.post('/api/user/replace', model);
     }
 }
