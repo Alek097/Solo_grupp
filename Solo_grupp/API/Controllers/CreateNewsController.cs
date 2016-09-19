5167e521e175572f28ee347f1fbb8def2bed9162
Alek097 <petrovskii097@gmail.com>
2016-09-19 22:23:56 +0300

    Initial commit: added CreateNews/CreateNewsController and createNewsStyle.less and CreateNewsController.cs and CreateNews/CreateNewsView.html

---
+++ Solo_grupp/API/Controllers/CreateNewsController.cs
@@ -0,0 +1,42 @@
+﻿namespace API.Controllers
+{
+	#region Using
+	using Data.Models;
+	using Filters;
+	using Models;
+	using System;
+	using System.IO;
+	using System.Net.Http;
+	using System.Threading.Tasks;
+	using System.Web.Http;
+	#endregion
+	[Authorize]
+	public class CreateNewsController : ApiController
+    {
+		//public async Task<UploadResult> UploadImage()
+		//{
+		//	if (!Request.Content.IsMimeMultipartContent())
+		//	{
+		//		return new UploadResult()
+		//		{
+		//			IsUploading = false
+		//		};
+		//	}
+		//	var provider = new MultipartMemoryStreamProvider();
+		//	// путь к папке на сервере
+		//	string root = System.Web.HttpContext.Current.Server.MapPath("~/../temp_img/");
+		//	await Request.Content.ReadAsMultipartAsync(provider);
+
+		//	foreach (var file in provider.Contents)
+		//	{
+		//		string fileName = Guid.NewGuid().ToString();
+		//		byte[] fileArray = await file.ReadAsByteArrayAsync();
+
+		//		using (FileStream fs = new FileStream(root + fileName, FileMode.Create))
+		//		{
+		//			await fs.WriteAsync(fileArray, 0, fileArray.Length);
+		//		}
+		//	}
+		//}
+    }
+}
---
+++ Solo_grupp/API/Scripts/Pages/News/CreateNews/CreateNewsController.ts
@@ -0,0 +1,11 @@
+﻿export class CreateNewsController {
+    public submit(): void {
+        let input: JQuery = angular.element('input[type=file]');
+
+        let data = new FormData();
+
+        for (let i: number = 0; i < input.length; i++) {
+            data.append('file' + i, input[i]);
+        }
+    }
+}
