426faf0424314e5c090c4e02a8bfd465f77880a4
Alek097 <petrovskii097@gmail.com>
2016-09-22 21:16:15 +0300

    Initial commit: added logic

--- Solo_grupp/API/Controllers/CreateNewsController.cs
+++ Solo_grupp/API/Controllers/CreateNewsController.cs
@@ -5,38 +5,83 @@
 	using Filters;
 	using Models;
 	using System;
+	using System.Collections.Generic;
 	using System.IO;
 	using System.Net.Http;
 	using System.Threading.Tasks;
+	using System.Web;
 	using System.Web.Http;
 	#endregion
 	[Authorize]
 	public class CreateNewsController : ApiController
-    {
-		//public async Task<UploadResult> UploadImage()
-		//{
-		//	if (!Request.Content.IsMimeMultipartContent())
-		//	{
-		//		return new UploadResult()
-		//		{
-		//			IsUploading = false
-		//		};
-		//	}
-		//	var provider = new MultipartMemoryStreamProvider();
-		//	// путь к папке на сервере
-		//	string root = System.Web.HttpContext.Current.Server.MapPath("~/../temp_img/");
-		//	await Request.Content.ReadAsMultipartAsync(provider);
+	{
+		[HttpPost]
+		[Resolution(ResolutionType.AddNews)]
+		public async Task<UploadResult> UploadImage()
+		{
+			if (!Request.Content.IsMimeMultipartContent())
+			{
+				return new UploadResult()
+				{
+					IsUploading = false
+				};
+			}
 
-		//	foreach (var file in provider.Contents)
-		//	{
-		//		string fileName = Guid.NewGuid().ToString();
-		//		byte[] fileArray = await file.ReadAsByteArrayAsync();
+			List<string> urls = new List<string>();
+			#region images extensions
+			List<string> extensions = new List<string>(new string[]
+			{
+				".JPG",
+				".JFIF",
+				".JPE",
+				".JPEG",
+				".GIF",
+				".PNG",
+				".SVG",
+				".SVGZ",
+				".TIF",
+				".TIFF",
+				".ICO",
+				".WBMP",
+				".WEBP"
+			});
+			#endregion
 
-		//		using (FileStream fs = new FileStream(root + fileName, FileMode.Create))
-		//		{
-		//			await fs.WriteAsync(fileArray, 0, fileArray.Length);
-		//		}
-		//	}
-		//}
-    }
+			var provider = new MultipartMemoryStreamProvider();
+
+			string root = HttpContext.Current.Server.MapPath("~/Bundles/app/img/tmp");
+
+
+			await Request.Content.ReadAsMultipartAsync(provider);
+
+
+			foreach (var file in provider.Contents)
+			{
+				string fileExtension = Path.GetExtension(file.Headers.ContentDisposition.FileName.Trim('\"'));
+
+				if (extensions.Contains(fileExtension.ToUpper()))
+				{
+					string fileName = string.Format("{0}_{1}{2}",
+						Guid.NewGuid(),
+						Guid.NewGuid(),
+						fileExtension);
+
+					urls.Add(string.Format("../../../Bundles/app/img/tmp/{0}", fileName));
+
+					byte[] fileArray = await file.ReadAsByteArrayAsync();
+
+					using (FileStream fs = new FileStream(Path.Combine(root, fileName), FileMode.Create))
+					{
+						await fs.WriteAsync(fileArray, 0, fileArray.Length);
+					}
+				}
+			}
+
+			return new UploadResult()
+			{
+				IsUploading = true,
+				Urls = urls
+			};
+		}
+	}
 }
