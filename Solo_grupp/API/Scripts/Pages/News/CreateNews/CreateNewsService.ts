f0479d4f47f0595790a33f542855aca8e4169a59
Alek097 <petrovskii097@gmail.com>
2016-09-22 17:45:56 +0300

    Initial commit: added CreateNewsService.ts

---
+++ Solo_grupp/API/Scripts/Pages/News/CreateNews/CreateNewsService.ts
@@ -0,0 +1,16 @@
+﻿export class CreateNewsService {
+    public static $inject: string[] =
+    [
+        '$http'
+    ];
+
+    constructor(
+        private http: ng.IHttpService
+    ) {
+
+    }
+
+    public uploadImage(data: FormData): ng.IHttpPromise<string[]> {
+        return this.http.post('/api/CreateNews/UploadImage', data);
+    }
+}
