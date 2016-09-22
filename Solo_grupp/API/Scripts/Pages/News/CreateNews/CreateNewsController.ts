acafd1fc0b7d98e30757f282f5e4b932c27e0ab4
Alek097 <petrovskii097@gmail.com>
2016-09-22 17:55:36 +0300

    Initial commit: added logic in uploadFile method

--- Solo_grupp/API/Scripts/Pages/News/CreateNews/CreateNewsController.ts
+++ Solo_grupp/API/Scripts/Pages/News/CreateNews/CreateNewsController.ts
@@ -1,15 +1,19 @@
-﻿export class CreateNewsController {
+﻿import {CreateNewsService} from './CreateNewsService.ts'
+
+export class CreateNewsController {
 
     public static $inject: string[] =
     [
-        '$timeout'
+        '$timeout',
+        'createNewsService'
     ];
 
     public imgUrls: string[] = [];
     public content: string = null;
 
     constructor(
-        private timeout: ng.ITimeoutService
+        private timeout: ng.ITimeoutService,
+        private service: CreateNewsService
     ) {
 
     }
@@ -25,6 +29,21 @@
     }
 
     public uploadFile(): void {
+        let tmpVar: any = document.getElementById('uploadFile');
+        let files = tmpVar.files;
 
+        let data: FormData = new FormData();
+        for (let i: number = 0; i < files.length; i++) {
+            data.append('file' + i, files[i]);
+        }
+
+        this.service.uploadImage(data)
+            .success((data: string[]) => {
+                for (let i: number = 0; i < data.length; i++) {
+                    this.imgUrls[this.imgUrls.length] = data[i];
+
+                    this.timeout();
+                }
+            });
     }
 }
