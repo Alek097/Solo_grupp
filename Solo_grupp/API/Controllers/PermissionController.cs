5a8a2d64222a397abef2da672594334f96393f79
Alek097 <petrovskii097@gmail.com>
2016-09-26 15:41:49 +0300

    Initial commit: added PermissionController

--- Solo_grupp/API/Controllers/PermissionController.cs
+++ Solo_grupp/API/Controllers/PermissionController.cs
@@ -9,6 +9,43 @@ namespace API.Controllers
 	public class PermissionController : ApiController
 	{
 		[Resolution(ResolutionType.AddNews)]
+		[HttpGet]
 		public void IsAddNews() { }
+
+		[Resolution(ResolutionType.EditRankOfUser)]
+		[HttpGet]
+		public void IsEditRankOfUser() { }
+
+		[Resolution(ResolutionType.AddProduct)]
+		[HttpGet]
+		public void IsAddProduct() { }
+
+		[Resolution(ResolutionType.EditProduct)]
+		[HttpGet]
+		public void IsEditProduct() { }
+
+		[Resolution(ResolutionType.DeleteProduct)]
+		[HttpGet]
+		public void IsDeleteProduct() { }
+
+		[Resolution(ResolutionType.DeleteNews)]
+		[HttpGet]
+		public void IsDeleteNews() { }
+
+		[Resolution(ResolutionType.EditNews)]
+		[HttpGet]
+		public void IsEditNews() { }
+
+		[Resolution(ResolutionType.AddComment)]
+		[HttpGet]
+		public void IsAddComment() { }
+
+		[Resolution(ResolutionType.DeleteUserComment)]
+		[HttpGet]
+		public void IsDeleteUserComment() { }
+
+		[Resolution(ResolutionType.EditUserComment)]
+		[HttpGet]
+		public void IsEditUserComment() { }
 	}
 }
