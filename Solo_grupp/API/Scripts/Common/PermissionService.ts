e5eb6f69cf242df239f0b90a36bd41fdda449777
Alek097 <petrovskii097@gmail.com>
2016-09-26 15:28:33 +0300

    Initial commit: added PermissionService

---
+++ Solo_grupp/API/Scripts/Common/PermissionService.ts
@@ -0,0 +1,15 @@
+﻿export class PermissionService {
+    public static $inject: string[] = [
+        '$http'
+    ];
+
+    constructor(
+        private http: ng.IHttpService
+    ) {
+
+    }
+
+    public IsResolution(resolution: string): ng.IHttpPromise<any> {
+        return this.http.get('/api/Permission/is' + resolution);
+    }
+}
