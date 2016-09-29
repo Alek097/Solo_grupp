345e0d79b6e800b88054aa9d1b255aefe07866f3
Alek097 <petrovskii097@gmail.com>
2016-09-29 05:29:41 +0300

    Initial commit: added Title property

--- Solo_grupp/API.Data/Models/News.cs
+++ Solo_grupp/API.Data/Models/News.cs
@@ -7,6 +7,7 @@
 	public class News
 	{
 		public Guid Id { get; set; }
+		public string Title { get; set; }
 		public string Content { get; set; }
 		public virtual User User { get; set; }
 		public virtual ICollection<Image> Images { get; set; }
