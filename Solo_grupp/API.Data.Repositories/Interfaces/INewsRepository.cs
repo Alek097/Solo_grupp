448eec7b07fda4e465ba01e21908e26564207728
Alek097 <petrovskii097@gmail.com>
2016-10-02 16:31:16 +0300

    Initial commit: added INewsRepository and NewsRepository with GetNews method

---
+++ Solo_grupp/API.Data.Repositories/Interfaces/INewsRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Data.Repositories.Interfaces
+{
+	#region Using
+	using API.Models;
+	using Models;
+	using System;
+	using System.Threading.Tasks;
+	#endregion
+	public interface INewsRepository : IDisposable
+	{
+		Task<ControllerResult<News>> GetNews(Guid id);
+	}
+}
---
+++ Solo_grupp/API.Data.Repositories/NewsRepository.cs
@@ -0,0 +1,54 @@
+﻿namespace API.Data.Repositories
+{
+	#region Using
+	using API.Models;
+	using System;
+	using System.Threading.Tasks;
+	using Interfaces;
+	using Models;
+	using Logging;
+	#endregion
+	public class NewsRepository : INewsRepository
+	{
+		private readonly IContext context;
+		private readonly ILogger logger;
+		public NewsRepository(IContext context, ILogger logger)
+		{
+			this.context = context;
+			this.logger = logger;
+		}
+
+		public async Task<ControllerResult<News>> GetNews(Guid id)
+		{
+			return await new Task<ControllerResult<News>>(() =>
+			{
+				ControllerResult<News> result = new ControllerResult<News>();
+
+				News news = context.Get<News, Guid>(id);
+
+				if (news == null)
+				{
+					result.IsSucces = false;
+					result.Message = "Новость не найдена!";
+
+					this.logger.WriteError(string.Format("Новость по id {0} не найдена.", id));
+				}
+				else
+				{
+					news.Images = null;
+					news.User = null;
+
+					result.IsSucces = true;
+					result.Value = news;
+				}
+
+				return result;
+			});
+		}
+
+		public void Dispose()
+		{
+			this.context.Dispose();
+		}
+	}
+}
