cb0b7bc4399d5fab143e726b5c7f7575f451054d
Alek097 <petrovskii097@gmail.com>
2016-10-02 16:06:49 +0300

    Initial commit: added NewsController

---
+++ Solo_grupp/API/Controllers/NewsController.cs
@@ -0,0 +1,17 @@
+﻿namespace API.Controllers
+{
+	#region Using
+	using Data.Models;
+	using System;
+	using System.Threading.Tasks;
+	using System.Web.Http;
+	#endregion
+	public class NewsController : ApiController
+	{
+		[HttpGet]
+		public async Task<News> GetNews(Guid id)
+		{
+
+		}
+	}
+}
