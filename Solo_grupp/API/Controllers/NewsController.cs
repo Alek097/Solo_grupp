30d98fed0bf2b80981684a4c4bbd3f51c9f71ee6
Alek097 <petrovskii097@gmail.com>
2016-10-02 16:37:23 +0300

    Initial commit: fix GetNews method and added Dispose method

--- Solo_grupp/API/Controllers/NewsController.cs
+++ Solo_grupp/API/Controllers/NewsController.cs
@@ -2,16 +2,29 @@
 {
 	#region Using
 	using Data.Models;
+	using Data.Repositories.Interfaces;
+	using Models;
 	using System;
 	using System.Threading.Tasks;
 	using System.Web.Http;
 	#endregion
 	public class NewsController : ApiController
 	{
-		[HttpGet]
-		public async Task<News> GetNews(Guid id)
+		private readonly INewsRepository repository;
+		public NewsController(INewsRepository repository)
 		{
+			this.repository = repository;
+		}
+		[HttpGet]
+		public async Task<ControllerResult<News>> GetNews(Guid id)
+		{
+			return await this.repository.GetNews(id);
+		}
 
+		public new void Dispose()
+		{
+			this.repository.Dispose();
+			base.Dispose();
 		}
 	}
 }
