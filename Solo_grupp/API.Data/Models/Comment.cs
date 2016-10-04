ac6a4d85ea1426201249bde18aa6c64636c6d17f
Alek097 <petrovskii097@gmail.com>
2016-10-04 20:58:22 +0300

    Initial commit: added Comment model

---
+++ Solo_grupp/API.Data/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Data.Models
+{
+	#region Using
+	using System;
+	using System.Collections.Generic;
+	#endregion
+	public class Comment
+	{
+		public Guid Id { get; set; }
+		public string Text { get; set; }
+		public DateTime CreateDate { get; set; }
+		public virtual User Author { get; set; }
+		public virtual ICollection<Comment> Comments { get; set; }
+	}
+}
