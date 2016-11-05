83789881567b03ae846fbf0cb71ebce1ba67018f
Alek097 <petrovskii097@gmail.com>
2016-11-05 15:17:41 +0300

    Initial commit: added Comment.ts

---
+++ Solo_grupp/API/Scripts/Common/Models/Comment.ts
@@ -0,0 +1,9 @@
+﻿import {User} from './User.ts'
+
+export class Comment {
+    public Id: string = null;
+    public Text: string = null;
+    public CreateDate: Date = null;
+    public Author: User = null;
+    public Comments: Comment[] = [];
+}
