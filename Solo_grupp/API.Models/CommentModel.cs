6aa7da8774eeef6bce31d1a2f5072e7d23c04f03
Alek097 <petrovskii097@gmail.com>
2016-11-05 15:55:27 +0300

    fix

--- Solo_grupp/API.Models/CommentModel.cs
+++ Solo_grupp/API.Models/CommentModel.cs
@@ -4,12 +4,12 @@
 	using System;
 	using System.Collections.Generic;
 	#endregion
-	public class Comment
+	public class CommentModel
 	{
 		public Guid Id { get; set; }
 		public string Text { get; set; }
 		public DateTime CreateDate { get; set; }
 		public virtual UserInformation Author { get; set; }
-		public virtual ICollection<Comment> Comments { get; set; }
+		public virtual ICollection<CommentModel> Comments { get; set; }
 	}
 }
