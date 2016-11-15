d2df105cba89286a73fbc1ba4b7b41e43dd7ffbc
Alek097 <petrovskii097@gmail.com>
2016-11-15 06:10:29 +0300

    Initial commit: added authentication

--- Solo_grupp/API/Scripts/Common/ShowComments/ShowCommentsController.ts
+++ Solo_grupp/API/Scripts/Common/ShowComments/ShowCommentsController.ts
@@ -1,10 +1,30 @@
 ﻿import {Comment} from '../Models/Comment'
+import {User} from '../Models/User.ts'
+import {AuthorizeService} from '../Menu/AuthorizeService.ts'
 
 export class ShowCommentsController {
+    public static $inject: string[] = [
+        'authorizeService'
+    ];
+
     public isAuth: boolean = false;
     public isRender: boolean = false;
     public Comments: Comment[] = [];
     public innerLevel: number = 0;
+
+    constructor(
+        authorizeService: AuthorizeService
+    ) {
+        authorizeService.Authentification()
+            .success((data: User) => {
+                if (data == undefined) {
+                    this.isAuth = false;
+                }
+                else {
+                    this.isAuth = true;
+                }
+            });
+    }
 
     public send(id: string): void {
         
